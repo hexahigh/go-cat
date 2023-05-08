1b656ace4c9a3ace20781bd82a4b21c30cbf27a2
Boof <97455552+hexahigh@users.noreply.github.com>
2023-05-08 12:03:16 +0200

    Update main.go

--- main.go
+++ main.go
@@ -9,10 +9,11 @@ import (
 	"os"
 )
 
-var hexFlag bool
+var hexFlag, hexrawFlag bool
 
 func init() {
 	flag.BoolVar(&hexFlag, "hex", false, "Usage: --hex to use hexadecimal for the output")
+	flag.BoolVar(&hexrawFlag, "hexraw", false, "Usage: --hexraw to use hexadecimal for the output")
 }
 
 func main() {
@@ -24,6 +25,9 @@ func main() {
 	} else if hexFlag {
 		path := flag.Arg(0)
 		readLinesHex(path)
+	} else if hexrawFlag {
+		path := flag.Arg(0)
+		readLinesHexraw(path)
 	} else {
 		path := flag.Arg(0)
 		readLines(path)
@@ -67,6 +71,31 @@ func readLinesHex(path string) ([]string, error) {
 
 		// Convert the chunk to hexadecimal and print it
 		fmt.Printf("%s", hex.Dump(buf))
+	}
+	return nil, nil
+}
+
+func readLinesHexraw(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	// Read the file in chunks
+	buf := make([]byte, 256) // Adjust the chunk size as needed
+	for {
+		_, err := reader.Read(buf)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println(err)
+			}
+			break
+		}
+
+		// Convert the chunk to hexadecimal and print it
+		fmt.Printf("%s", hex.EncodeToString(buf))
 	}
 	return nil, nil
 }
