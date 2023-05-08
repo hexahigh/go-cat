79d2046ea1e9ffdb38e0e36917d13910ecf019fa
Boof <97455552+hexahigh@users.noreply.github.com>
2023-05-08 10:20:44 +0200

    Update main.go

--- main.go
+++ main.go
@@ -2,15 +2,30 @@ package main
 
 import (
 	"bufio"
+	"encoding/hex"
+	"flag"
 	"fmt"
+	"io"
 	"os"
 )
 
+var hexFlag bool
+
+func init() {
+	flag.BoolVar(&hexFlag, "hex", false, "Usage: --hex to use hexadecimal for the output")
+}
+
 func main() {
-	if len(os.Args[1:]) > 1 || len(os.Args[1:]) == 0 {
-		fmt.Println("Too many or too few arguments specified")
+	flag.Parse()
+	pathOpt := flag.Arg(1)
+	fmt.Println(pathOpt)
+	if len(os.Args[1:]) == 0 {
+		fmt.Println("Too few arguments specified")
+	} else if hexFlag {
+		path := flag.Arg(0)
+		readLinesHex(path)
 	} else {
-		path := os.Args[1]
+		path := flag.Arg(0)
 		readLines(path)
 	}
 
@@ -29,4 +44,29 @@ func readLines(path string) ([]string, error) {
 		fmt.Println(scanner.Text())
 	}
 	return lines, scanner.Err()
+}
+
+func readLinesHex(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	// Read the file in chunks
+	buf := make([]byte, 256) // Adjust the chunk size as needed
+	for {
+		_, err := reader.Read(buf)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println(err)
+			}
+			break
+		}
+
+		// Convert the chunk to hexadecimal and print it
+		fmt.Printf("%s", hex.Dump(buf))
+	}
+	return nil, nil
 }
